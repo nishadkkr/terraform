a31650a9c280b0c04095a1da2945752d582e3a0c
Nishad P <nishadntechit@gmail.com>
2023-01-18 20:29:06 +0530

    Seperated tf files into dirs

---
+++ hello-world/mani.tf
@@ -0,0 +1,15 @@
+terraform {
+  # This module is now only being tested with Terraform 0.13.x. However, to make upgrading easier, we are setting
+  # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
+  # forwards compatible with 0.13.x code.
+  required_version = ">= 0.12.26"
+}
+
+# website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
+output "hello_world" {
+  value = "Hello, This terraform script is to show demo."
+}
+
+output "Important_Message" {
+    value = "You can see this hello-world script on my github repo and also able to clone or fork it.Thank You !"
+}
